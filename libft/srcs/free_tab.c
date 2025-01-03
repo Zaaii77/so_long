3b1bc149a0260e624b9d299ef76e1159544301f9
Louis Watelle <lowatell@fu-r9-p6.s19.be>
2025-01-03 16:13:37 +0100

    libft with fix

--- libft/srcs/free_tab.c
+++ libft/srcs/free_tab.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:24:17 by lowatell          #+#    #+#             */
-/*   Updated: 2024/12/17 10:37:58 by lowatell         ###   ########.fr       */
+/*   Updated: 2024/11/25 19:25:18 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	free_tab(char **tab)
 {
 	int	i;
 
-	if (!tab || !*tab)
+	if (!tab)
 		return ;
 	i = 0;
 	while (tab[i])
